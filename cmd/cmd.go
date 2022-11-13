e48c2bbe138b5bb821ba4e73e1190c1af8cfb5ce
Chris Wolf <chriswolfdesign@gmail.com>
2022-11-13 14:21:51 -0500

    Server can now run a "greeting" endpoint

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -19,4 +25,41 @@ func main() {
 	fmt.Println("DB:", PSQL_DB)
 	fmt.Println("TABLE:", PSQL_TABLE)
 	fmt.Println("HOST:", PSQL_HOST)
+
+	db, err := createDatabase(PSQL_HOST, PSQL_PORT, PSQL_USER, PSQL_PASSWORD, PSQL_DB)
+	defer db.Close()
+
+	if err != nil {
+		log.Println("ERROR ACCESSING DATABASE")
+		os.Exit(1)
+	}
+
+	log.Println("Contacted PSQL successfully")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/greet", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to our TODO List")
+	})
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://foo.com", "http://foo.com:8080"},
+		AllowCredentials: true,
+		Debug:            true,
+	})
+
+	handler := c.Handler(mux)
+
+	log.Println("ATTEMPTING TO START SERVER")
+
+	err = http.ListenAndServe(":8000", handler)
+	if err != nil {
+		log.Fatal("ERROR STARTING SERVER:", err)
+		os.Exit(1)
+	}
+}
+
+func createDatabase(host, port, user, password, dbName string) (*sql.DB, error) {
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+	return sql.Open("postgres", psqlconn)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module todo-back
 
 go 1.19
+
+require (
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/rs/cors v1.8.2 // indirect
+)
