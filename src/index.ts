8a9ff05fc3315a66272536d1d35c6975550dad17
chriswolf <chriswolfdesign@gmail.com>
2024-05-06 06:30:22 -0400

    Adds handler for getting a single todo item

--- src/index.ts
+++ src/index.ts
@@ -6,19 +6,19 @@ import * as mongoose from "mongoose";
 import Todo from "./model/todo";
 import statusCode from "http-status-codes";
 import getAllTodoHandler from "./handlers/get-all-todo-handler";
+import getSingleTodoHandler from "./handlers/get-single-todo-handler";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 2000;
 
-app.get("/", indexHandler);
-
-app.get("/check", healthCheckHandler);
-
 mongoose.connect("mongodb://127.0.0.1:27017/local").then(r => console.log("Connected to database"));
 
+app.get("/", indexHandler);
+app.get("/check", healthCheckHandler);
 app.get("/todos", getAllTodoHandler);
+app.get("/todos/:id", getSingleTodoHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
