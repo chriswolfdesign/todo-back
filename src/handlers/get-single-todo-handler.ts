052512f1b3ee151affeefc3e8312313fab35c343
Chris Wolf <chriswolfdesign@gmail.com>
2024-05-06 06:31:26 -0400

    Adds handler for getting a single todo item (#6)

---
+++ src/handlers/get-single-todo-handler.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express";
+import Todo from "../model/todo";
+import statusCode from "http-status-codes";
+
+export default async function getSingleTodoHandler(req: Request, res: Response) {
+    const id = req.params.id;
+
+    const todo = await Todo.findOne({_id: id});
+
+    if (todo === null) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(statusCode.BAD_REQUEST).json({
+            "message": "could not find todo with that id"
+        });
+        return;
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(statusCode.OK).json(todo);
+};
---
+++ test/handlers/get-single-todo-handler.test.ts
@@ -0,0 +1,59 @@
+import Todo from "../../src/model/todo";
+import {createRequest, createResponse} from "node-mocks-http";
+import getSingleTodoHandler from "../../src/handlers/get-single-todo-handler";
+import statusCode from "http-status-codes";
+
+const mockingoose = require("mockingoose");
+
+describe("Get single todo handler tests", () => {
+    describe("there is no todo with that id", () => {
+        it("should return bad request with error message", async () => {
+            mockingoose(Todo).toReturn(null, "findOne");
+
+            const mockRequest = createRequest({
+                method: "GET",
+                url: "/todos",
+                params: [
+                    {
+                        id: "1"
+                    }
+                ]
+            });
+
+            const mockResponse = createResponse();
+
+            await getSingleTodoHandler(mockRequest, mockResponse);
+            expect(mockResponse.statusCode).toBe(statusCode.BAD_REQUEST);
+            expect(mockResponse._getJSONData()).toStrictEqual({
+                "message": "could not find todo with that id"
+            });
+        });
+    });
+
+    describe("there is a todo with that id", () => {
+        it("should return status ok with that todo", async () => {
+            const doc = {
+                _id: "6637e890ada4275ce9ec5a26",
+                text: "testing",
+                completed: true,
+            };
+
+            mockingoose(Todo).toReturn(doc, "findOne");
+
+            const mockRequest = createRequest({
+                method: "GET",
+                url: "/todos",
+                params: {
+                    id: "6637e890ada4275ce9ec5a26"
+                }
+            });
+
+            const mockResponse = createResponse();
+
+            await getSingleTodoHandler(mockRequest, mockResponse);
+
+            expect(mockResponse.statusCode).toBe(statusCode.OK);
+            expect(mockResponse._getJSONData()).toStrictEqual(doc);
+        });
+    });
+});
